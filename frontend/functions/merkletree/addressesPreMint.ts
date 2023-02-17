ae15b92a2240df7a0b1ab4a9ab32d3f67288ff05
yasuyasu <faithlessboy007@yahoo.co.jp>
2023-02-17 10:10:02 +0900

    update

--- frontend/functions/merkletree/addressesPreMint.ts
+++ frontend/functions/merkletree/addressesPreMint.ts
@@ -1,4 +1,5 @@
 // PreMintのaddressとalCountの対応表
 export const addressAndAlCountMapPreMint = new Map<string, number>([
   ['0x29259AB48215239dBE1bc1e7bFCC818EB426ad7B', 1],
+  ['0x2382E6C9Cde357A3fA3ECdD255DefD3499e572F4', 1],
 ])
